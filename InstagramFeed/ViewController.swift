608393a9586df4bfe254112e052b9220467abf7a
Cassio Mo <cassiomo@gmail.com>
2015-07-25 18:52:06 -0700

    Initial Commit

---
+++ InstagramFeed/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InstagramFeed
+//
+//  Created by Mo, Kevin on 7/25/15.
+//  Copyright (c) 2015 Mo, Kevin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
