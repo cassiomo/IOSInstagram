12599cfbfaad65fe4379a89bd1ce021e7e424af6
Cassio Mo <cassiomo@gmail.com>
2015-09-16 20:26:29 -0700

    first commit

---
+++ InstagramFeed/PhotoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoTableViewCell.swift
+//  InstagramFeed
+//
+//  Created by Mo, Kevin on 7/25/15.
+//  Copyright (c) 2015 Mo, Kevin. All rights reserved.
+//
+
+import UIKit
+
+class PhotoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ InstagramFeed/PhotoViewController.swift
@@ -0,0 +1,9 @@
+//
+//  PhotoViewController.swift
+//  InstagramFeed
+//
+//  Created by Mo, Kevin on 7/25/15.
+//  Copyright (c) 2015 Mo, Kevin. All rights reserved.
+//
+
+import Foundation
